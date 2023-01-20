76d15a42f052ce286229cd4f5acf4d31d92630b9
mbstevenson96 <mbstevenson96@gmail.com>
2023-01-20 14:01:44 -0500

    add content to wedding party page

--- src/pages/WeddingParty/WeddingParty.jsx
+++ src/pages/WeddingParty/WeddingParty.jsx
@@ -1,8 +1,59 @@
 const WeddingParty = () => {
   return ( 
-    <div>
-      <h1>This is the Wedding Party Component</h1>
-    </div>
+    <>
+      <div>
+        <h1>Bridesmaids</h1>
+        <ul>
+          <li>Maddie Stevenson | Maid of Honor</li>
+          <li>Jennie Berrie | Bridesmaide</li>
+          <li>Amber Woodard | Bridesmaide</li>
+          <li>Samantha Searcy | Bridesmaide</li>
+          <li>Nicole Keppel | Bridesmaide</li>
+          <li>Makayla Richardson | Bridesmaide</li>
+          <li>Emma Browning | Bridesmaide</li>
+          <li>Sarah Sapp | Bridesmaide</li>
+          <li>Gabby Steeley | Bridesmaide</li>
+        </ul>
+      </div>
+      <div>
+        <h1>Groomsmen</h1>
+        <ul>
+          <li>Evan Debo | Best Man</li>
+          <li>Sam Stevenson | Groomsman</li>
+          <li>Ian Karvel | Groomsman</li>
+          <li>Larry Jeffers | Groomsman</li>
+          <li>Nick Riegel | Groomsman</li>
+          <li>Alex Provonsha | Groomsman</li>
+          <li>Neil Fischer | Groomsman</li>
+          <li>Seth Forbes | Groomsman</li>
+        </ul>
+      </div>
+      <div>
+        <h1>Extended Wedding Party</h1>
+        <ul>
+          <li>Charlie | Best Dog</li>
+          <li>Taylor Acuff | Ring Bearer</li>
+          <li>Ellia Goodhart | Flower Girl</li>
+          <li>Ada Goodhart | Flower Girl</li>
+        </ul>
+      </div>
+      <div>
+        <h1>Family of the Groom</h1>
+        <ul>
+          <li>Tim Stevenson | Father of the Groom</li>
+          <li>Linda Stevenson | Mother of the Groom</li>
+          <li>Laura Iskra | Grandmother of the Groomn</li>
+        </ul>
+      </div>
+      <div>
+        <h1>Family of the Bride</h1>
+        <ul>
+          <li>Mike Steeley | Father of the Bride</li>
+          <li>Christie Steeley | Mother of the Bride</li>
+          <li>Homie Steeley | Grandfather of the Bride</li>
+        </ul>
+      </div>
+    </>
   );
 }
 
