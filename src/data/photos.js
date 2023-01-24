fcbe499823967bdde36f9c6e0539b882c7227eed
mbstevenson96 <mbstevenson96@gmail.com>
2023-01-24 10:21:31 -0500

    stub up photos data

---
+++ src/data/photos.js
@@ -0,0 +1,22 @@
+const photos = [
+  {
+    image: ''
+  },
+  {
+    image: ''
+  },
+  {
+    image: ''
+  },
+  {
+    image: ''
+  },
+  {
+    image: ''
+  },
+  {
+    image: ''
+  },
+]
+
+export default photos;
