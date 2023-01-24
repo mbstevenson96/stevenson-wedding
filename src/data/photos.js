80d23b7aa934b5b01e0a8cbc2a5acd3980dafa9f
mbstevenson96 <mbstevenson96@gmail.com>
2023-01-24 10:38:44 -0500

    add photos to photo data and create photolist component

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,21 +1,33 @@
 const photos = [
   {
-    image: ''
+    image: 'https://i.imgur.com/ORStGKm.jpg'
   },
   {
-    image: ''
+    image: 'https://i.imgur.com/Iuxx0Lq.jpg'
   },
   {
-    image: ''
+    image: 'https://i.imgur.com/YFmc7eU.jpg'
   },
   {
-    image: ''
+    image: 'https://i.imgur.com/kTWzpg5.jpg'
   },
   {
-    image: ''
+    image: 'https://i.imgur.com/zMdG5W3.jpg'
   },
   {
-    image: ''
+    image: 'https://i.imgur.com/KvMwDp9.jpg'
+  },
+  {
+    image: 'https://i.imgur.com/bueGoLT.jpg'
+  },
+  {
+    image: 'https://i.imgur.com/Y7gg5xk.jpg'
+  },
+  {
+    image: 'https://i.imgur.com/nR6K93p.jpg'
+  },
+  {
+    image: 'https://i.imgur.com/2n5EJH4.jpg'
   },
 ]
 
