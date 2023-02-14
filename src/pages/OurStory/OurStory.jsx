4c12e23a42c60bb3b4013ee177b1b2d69775fd04
mbstevenson96 <mbstevenson96@gmail.com>
2023-02-13 21:29:09 -0500

    add lorem ipsum to Our Story page to style

--- src/pages/OurStory/OurStory.jsx
+++ src/pages/OurStory/OurStory.jsx
@@ -1,7 +1,12 @@
 const OurStory = () => {
   return ( 
     <div>
-      <h1>Our Story Coming Soon!</h1>
+      <h1>Our Story!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie metus eget nulla sollicitudin tempus at et ipsum. Duis elementum posuere mollis. Morbi sed ligula sodales, interdum mauris non, commodo mi. Sed tincidunt odio ut rutrum dictum. Vestibulum id molestie dolor, nec varius lorem. Integer lectus ligula, pellentesque eu ullamcorper in, malesuada non sem. Maecenas turpis odio, tincidunt a dignissim sed, finibus vitae quam. Sed auctor felis nulla, quis accumsan tellus tempor vel. Proin eros risus, sagittis nec elit dignissim, tempus pellentesque felis. Integer a quam arcu. Vestibulum pulvinar nisl vel dui tincidunt sollicitudin. Sed semper, enim ac pulvinar consectetur, eros nisi interdum magna, nec pharetra nibh ante lacinia purus. Donec malesuada, tortor ut condimentum interdum, metus purus suscipit est, et molestie libero odio sit amet elit. Maecenas fringilla justo tincidunt, hendrerit lacus a, fringilla ipsum. Ut sodales eu ex sit amet ultricies.
+
+        In ac vulputate ligula, quis mattis dui. Etiam posuere vitae purus sit amet hendrerit. Aenean dignissim velit a tortor interdum varius. Cras pretium felis vitae risus semper semper. Morbi id massa massa. Quisque efficitur turpis magna, molestie congue metus ornare eget. Mauris sed eleifend sapien. Nam at orci at sem vulputate efficitur. Proin vehicula sed velit pellentesque sollicitudin. Duis porttitor posuere massa, vitae commodo tellus pellentesque eu. Proin molestie ante lectus, quis finibus odio euismod sed. Aliquam commodo consectetur accumsan. Donec venenatis, ex ut consequat sagittis, tellus risus fermentum lorem, sed tempus elit sapien eget sapien. Maecenas rhoncus varius dolor sed sodales. Pellentesque venenatis arcu sed ante hendrerit interdum. 
+      </p>
     </div>
   );
 }
