a3dbb91ed775ad682cd10bff73d60c056137fb19
mbstevenson96 <mbstevenson96@gmail.com>
2023-02-21 11:46:32 -0500

    update home page info

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,13 +10,12 @@ const Home = () => {
         <div>
           <h2>WEDDING DAY</h2>
           <p>October 1, 2023</p>
-          <p>3:30pm</p>
           <p>White Oak Farms</p>
           <p>284 Spring Creek Highway, Medina, TN 38355</p>
         </div>
         <div>
-          <h2>3:30pm | Ceremony</h2>
-          <h2>4:00pm | Reception</h2>
+          <h2>4:30pm | Ceremony</h2>
+          <h2>5:00pm | Reception</h2>
         </div>
 
       </div>
