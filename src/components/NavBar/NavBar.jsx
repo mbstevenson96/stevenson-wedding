7aaf8a12c5ba40dbbbcae692e7dee09067ac13ec
mbstevenson96 <mbstevenson96@gmail.com>
2023-02-24 12:32:04 -0500

    change link colors in nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,25 @@
 import { Link } from "react-router-dom";
 import { Nav, Navbar } from "react-bootstrap";
 import styles from './NavBar.module.css'
-
 import "bootstrap/dist/css/bootstrap.min.css";
-  
+
   const NavBar = () => {
 
   return (
   <>
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" className={styles.container}>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Link to='/'>Home</Link>
-          <Link to='/our-story'>Our Story</Link>
-          <Link to='/photos'>Photos</Link>
-          <Link to='/wedding-party'>Wedding Party</Link>
-          <Link to='/faq'>FAQ</Link>
-          <Link to='/travel'>Travel</Link>
-          <Link to='/things-to-do'>Things to Do</Link>
-          <Link to='/registry'>Registry</Link>
-          <Link to='/rsvp'>RSVP</Link>
+          <Link className={styles.links} to='/'>Home</Link>
+          <Link className={styles.links} to='/our-story'>Our Story</Link>
+          <Link className={styles.links} to='/photos'>Photos</Link>
+          <Link className={styles.links} to='/wedding-party'>Wedding Party</Link>
+          <Link className={styles.links} to='/faq'>FAQ</Link>
+          <Link className={styles.links} to='/travel'>Travel</Link>
+          <Link className={styles.links} to='/things-to-do'>Things to Do</Link>
+          <Link className={styles.links} to='/registry'>Registry</Link>
+          <Link className={styles.links} to='/rsvp'>RSVP</Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
